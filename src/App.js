dcd43f4ec56116060d0f1bbcc5718474d59bdcf0
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-12-27 19:22:18 -0700

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
