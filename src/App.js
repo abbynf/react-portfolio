e28e1526e3a257b5d72a6778b1a5318672b8a43d
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-12-27 19:18:40 -0700

    set up react-router-dom navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      Hello 
+
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/contact" component={Contact} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
     </div>
   )
 }
