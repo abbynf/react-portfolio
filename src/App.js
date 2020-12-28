4458e21903f927fceb2bf2d1b37d57b667a39753
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-12-27 20:15:09 -0700

    added style to header nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
-function Header (){
-    return (
-        <div>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/portfolio">Portfolio</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </div>
-    )
+class Header extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            home: "nav-item active",
+            portfolio: "nav-item",
+            contact: "nav-item"
+        }
+    }
+    render() {
+
+        return (
+            <div className="sticky-top">
+                <nav className="navbar navbar-expand navbar-dark bg-dark">
+                    <div className="collapse navbar-collapse" id="navbarNav">
+
+                        <ul className="navbar-nav">
+                            <li className={this.state.home}>
+                                <Link to="/" className="nav-link" 
+                                onClick={() => this.setState({ home:"nav-item active", portfolio: "nav-item", contact: "nav-item" })}
+                                >Home</Link>
+                            </li>
+                            <li className={this.state.portfolio}>
+                                <Link to="/portfolio" className="nav-link"
+                                onClick={() => this.setState({ home:"nav-item", portfolio: "nav-item active", contact: "nav-item"})}
+                                >Portfolio</Link>
+                            </li>
+                            <li className={this.state.contact}>
+                                <Link to="/contact" className="nav-link"
+                                onClick={() => this.setState({ home:"nav-item", portfolio:"nav-item", contact:"nav-item active"})}
+                                >Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
 };
 
 export default Header;
