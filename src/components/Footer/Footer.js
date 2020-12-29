06cbb16748c173134ec8fe85abe95c5f47646d66
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-12-29 16:17:58 -0700

    added semantic html to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,12 +3,12 @@ import './style.css';
 
 function Footer() {
     return (
-        <div className="footer pb-2 pt-1">
-            <a href="https://github.com/abbynf" className="footer-link">abbynf</a>
-            <p className="m-0">Contact me at abbynfrandsen@gmail.com</p>
-
-
-        </div>
+        <footer>
+            <div className="footer pb-2 pt-1">
+                <a href="https://github.com/abbynf" className="footer-link">abbynf</a>
+                <p className="m-0">Contact me at abbynfrandsen@gmail.com</p>
+            </div>
+        </footer>
     )
 };
 
