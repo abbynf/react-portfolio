5e6d5316986a1e096625a8d9f478a0e03c6e6096
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-12-29 15:39:23 -0700

    added footer and style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
@@ -10,15 +11,15 @@ import './App.css';
 function App() {
   return (
     <div>
-
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route path="/contact" component={Contact} />
-        <Route path="/portfolio" component={Portfolio} />
-        <Route path="/" component={Home} />
-      </Switch>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/contact" component={Contact} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+function Footer() {
+    return (
+        <div className="footer pb-2 pt-1">
+            <a href="https://github.com/abbynf" className="footer-link">abbynf</a>
+            <p className="m-0">Contact me at abbynfrandsen@gmail.com</p>
+
+
+        </div>
+    )
+};
+
+export default Footer;
