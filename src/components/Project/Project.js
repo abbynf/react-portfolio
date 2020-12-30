764396df49482b69aef621b32c5082525536b670
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-12-30 15:55:20 -0700

    created project 1 list, imported new components

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,10 +1,66 @@
 import React, { Component } from 'react';
+import Cardtext from '../Cardtext';
+import TechItem from './../TechItem';
 import './style.css';
 
 class Project extends Component {
+    
     render() {
+        
+        console.log(this.props)
+        
         return (
-            <div className="project">Project</div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-shadow p-3">
+                        <div class="row">
+                            <div class="col-12 col-lg-6">
+                                <img class="card-img-top rounded" src={this.props.img1src}
+                                    alt={this.props.img1alt} />
+                            </div>
+                            <hr class="portHr d-block d-lg-none" />
+                            <div class="col-12 col-lg-6">
+                                <img class="card-img-top rounded" src={this.props.img2src}
+                                    alt={this.props.img2alt} />
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title font-weight-bold">{this.props.title}</h5>
+                            <div class="row card-opaque mb-3">
+                                <div class="col-md-6">
+                                    {this.props.paragraphs.map((paragraph) => {
+                                        return (
+                                            <Cardtext paragraphtext={paragraph} />
+                                        )
+                                    })}
+                                </div>
+
+                                <div class="col-md-6 mt-0 pl-md-5">
+                                    <h6 class="card-text font-weight-bold mt-4 mb-1 mt-md-0">Technologies used:</h6>
+
+                                    <div class="row">
+
+                                        <div class="col">
+                                            <ul class="card-text">
+                                                <TechItem text={this.props.techitems1}/>
+                                            </ul>
+
+                                        </div>
+                                        <div class="col">
+                                            <ul class="card-text">
+                                                <TechItem text={this.props.techitems2}/>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <a href={this.props.applicationLink} class="btn portfolioButton" target="_blank">Access application</a>
+                            <a href={this.props.repoLink} class="btn portfolioButton" target="_blank">Visit repository</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 };
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import Project from './../components/Project/';
+import img1 from './../assets/project-caps/national-parks/national-parks-app.png';
+import img2 from './../assets/project-caps/national-parks/national-parks-app-cards.png';
 
 class Portfolio extends Component {
+    constructor(){
+        super();
+        this.state = {
+            projects: [
+                {
+                    img1src: img1,
+                    img1alt: "Landing page of National Parks app",
+                    img2src: img2,
+                    img2alt: "Search results for Utah",
+                    title: "National Parks in the Pandemic",
+                    paragraphs: ["The best way to get away from the bustle of daily life while maintaining social distance is visiting any of the nation's beautiful parks. Choose a state to view all of the parks and status of the coronavirus.",
+                    "This is a front-end application that was created by me and two other developers. We utilized UIkit for adaptability to large and small screens.",
+                    "My role in the team centered around utilizing plain JavaScript and jQuery to make AJAX requests to the National Parks API and other functions of the app. I also designed the landing page."
+                    ],
+                    techitems1: ["HTML", "CSS", 'jQuery', 'AJAX Requests', 'UIKit'],
+                    techitems2: ["CSS Wand", "Meyer Web CSS reset", "Google Fonts", "Covid Tracking API", "National Park API"],
+                    applicationLink: "https://abbynf.github.io/covid-travel/",
+                    repoLink: "https://github.com/abbynf/covid-travel"
+                }
+            ]
+        }
+    }
 
     render() {
         return (
@@ -15,8 +39,18 @@ class Portfolio extends Component {
                 </header>
 
                 <main>
-                    <Project/>
-
+                    <Project 
+                        img1src={this.state.projects[0].img1src}
+                        img1alt={this.state.projects[0].img1alt}
+                        img2src={this.state.projects[0].img2src}
+                        img2alt={this.state.projects[0].img2alt}
+                        title={this.state.projects[0].title}
+                        paragraphs={this.state.projects[0].paragraphs}
+                        techitems1={this.state.projects[0].techitems1}
+                        techitems2={this.state.projects[0].techitems2}
+                        applicationLink={this.state.projects[0].applicationLink}
+                        repoLink={this.state.projects[0].repoLink}
+                    />
 
                 </main>
 
