6332ee88114318ecc26ef5c5478fc99160d83aad
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-12-30 10:50:49 -0700

    imported in about component

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Landing from './../components/Landing';
+import About from './../components/About';
+
+function Home() {
+    return (
+        <div>
+            <Landing />
+            <About />
+        </div>
+    )
+};
+
+export default Home;
