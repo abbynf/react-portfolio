b1d9950d458151d1580dc3f013cbd9477bfab62b
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-12-31 14:11:26 -0700

    created basic components for contact page

---
+++ src/components/Method/Method.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Method () {
+    return (
+        <div>Method</div>
+    )
+};
+
+export default Method;
---
+++ src/pages/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Method from './../components/Method';
+
+class Contact extends Component {
+
+    render(){
+        return (
+            <div>
+                <h1>Contact Me</h1>
+                <Method />
+            </div>
+
+        );
+    }
+}
+
+export default Contact;
