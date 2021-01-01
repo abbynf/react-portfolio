e3c2261a195e7f73d741bfb8ecddd65c7cadbfb6
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-12-31 21:46:38 -0700

    added main content to method component

--- src/components/Method/Method.js
+++ src/components/Method/Method.js
@@ -2,11 +2,28 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
-function Method (props) {
+function Method(props) {
     return (
-        <div>
-<FontAwesomeIcon icon={props.icon} />
-        <div>Method</div>
+        <div class="col-12 col-md-6 col-lg-3 mt-5">
+
+            <div class="row">
+                <div class="col">
+                    <a href={props.link} target="_blank"><FontAwesomeIcon icon={props.icon} /></a>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <h4 class="text">{props.title}</h4>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <p class="text">{props.handle}</p>
+                </div>
+            </div>
+
         </div>
     )
 };
