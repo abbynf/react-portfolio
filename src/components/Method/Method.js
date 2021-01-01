be86744173e73243e32158d68e886c9d80c32a02
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-12-31 22:12:02 -0700

    added styles

--- src/components/Method/Method.js
+++ src/components/Method/Method.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './style.css';
 
 
 function Method(props) {
     return (
-        <div class="col-12 col-md-6 col-lg-3 mt-5">
+        <div className="col-12 col-md-6 col-lg-3 mt-5">
 
-            <div class="row">
-                <div class="col">
-                    <a href={props.link} target="_blank"><FontAwesomeIcon icon={props.icon} /></a>
+            <div className="row">
+                <div className="col">
+                    <a href={props.link} target="_blank" className="contactIcon"><FontAwesomeIcon icon={props.icon} className="contactIcon"/></a>
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col">
-                    <h4 class="text">{props.title}</h4>
+            <div className="row">
+                <div className="col">
+                    <h4 className="text">{props.title}</h4>
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col">
-                    <p class="text">{props.handle}</p>
+            <div className="row">
+                <div className="col">
+                    <p className="text">{props.handle}</p>
                 </div>
             </div>
 
